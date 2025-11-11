a05cb8526138e7d304079d4175b536a5a4022b03
Quézia Jesuino <quezitabjq@gmail.com>
2025-11-11 10:36:27 -0400

    ajuests

--- client/vite.config.js
+++ client/vite.config.js
@@ -25,4 +25,11 @@ export default defineConfig({
       },
     },
   },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['cobrix.aleftec.com.br'],
+  },
 })
